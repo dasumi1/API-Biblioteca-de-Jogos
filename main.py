633b66fb2f2733e1d0f05bd14eb11b61895a1e51
dasumi1 <144630611+dasumi1@users.noreply.github.com>
2026-05-20 17:39:40 -0300

    Add CORS middleware to FastAPI app
    Added CORS middleware to the FastAPI application.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from rotas_login import login_router
 from rotas_jogos import jogos_router
